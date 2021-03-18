167b2c06e4138a4714e62df00abba153550b6688
=tzw <=tzw@qq.com>
2021-03-18 22:06:22 +0800

    20210318提交 spring cloud admin + spring cloud 链路追踪 sleuth+zipkin

---
+++ admin-service/src/main/java/com/example/adminservice/AdminServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.adminservice;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceApplication.class, args);
+    }
+
+}
